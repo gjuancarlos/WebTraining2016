f05700c322b876ebf9928edcdec34d4d96a13228
Juan Carlos Garcia <g.juancarlos@gmail.com>
2016-12-28 22:49:58 -0800

    commit

---
+++ CanvasSnapshooter/CanvasSnapshooter/Scripts/default.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+$(document).ready(function () {
+    $('#btnSnapshot').on('click', drawVideoFrame);
+})
+
+function drawVideoFrame() {
+    var canvas = document.getElementById('myCanvas');
+    var video = document.getElementById('myVideo');
+
+    canvas.getContext('2d').drawImage(video, 0, 0, 360, 240);
+}
